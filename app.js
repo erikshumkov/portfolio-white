cd7d0a071e9090fba085cff21fd170fdbf876401
Erik Shumkov <erikshumkov@gmail.com>
2020-03-09 13:36:35 +0100

    Updates

---
+++ app.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({
+  duration: 600,
+  easing: "ease-in",
+  viewFactor: 0.3
+});
+
+sr.reveal('.background');
+sr.reveal(".skills");
+sr.reveal(".projects", { duration: 900, easing: "ease", viewFactor: 0.3 });
