a9b0253cae34b03bffb9be14e8ec14d39b8787a8
Erik Shumkov <erikshumkov@gmail.com>
2020-04-11 18:46:05 +0200

    Add new projects

--- dist/app.js
+++ dist/app.js
@@ -7,3 +7,29 @@ window.sr = ScrollReveal({
 sr.reveal('.background');
 sr.reveal(".skills");
 sr.reveal(".projects", { duration: 900, easing: "ease", viewFactor: 0.3 });
+
+// barba.init({
+//   views: [
+//     {
+//       namespace: "home"
+//     },
+//     {
+//       namespace: "ecommerce"
+//     }
+//   ],
+//   transitions: [
+//     {
+//       leave({ current, next }) {
+//         let done = this.async();
+//         window.scrollTo(0, 0);
+//         const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+//         tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0, onComplete: done });
+//       },
+//       enter({ current, next }) {
+//         let done = this.async();
+//         const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+//         tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1, onComplete: done });
+//       }
+//     }
+//   ]
+// });
