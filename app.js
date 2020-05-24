b636a1c4b2ff56f14f54790fbb2057b26b80e9e4
Erik Shumkov <erikshumkov@gmail.com>
2020-05-24 22:17:16 +0200

    Updates

--- app.js
+++ app.js
@@ -1,62 +1,103 @@
-// Scroll reveal defaults
-window.sr = ScrollReveal({
-  duration: 600,
-  easing: "ease-in",
-  viewFactor: 0.3
-});
 
-// Reveal on scroll
-sr.reveal('.background');
-sr.reveal(".skills");
-sr.reveal(".show", { duration: 900, easing: "ease", viewFactor: 0.1 });
+if (window.location.href.indexOf("index") > -1) {
+  // Get follower div
+  const follower = document.getElementById("follower");
+  // Get project links
+  const projectLinks = document.querySelectorAll(".project-link");
+  const computers = document.querySelectorAll(".computer");
 
-// Show loader
-gsap.to(".loader-container", { opacity: 1, delay: 0.5, duration: 1 });
+  follower.innerHTML = `<p>Klicka för mer info! 📘</p>`;
 
-// Progress bar running from 0 - 100%
-gsap.to(".loader-show", { x: "105%", delay: 1.6, duration: 1, ease: "power2.out" });
+  // Make follower div follow the mouse when one of the project links get hovered
+  function follow(e) {
+    follower.style.left = e.pageX + 20 + "px";
+    follower.style.top = e.pageY + 20 + "px";
+    follower.style.visibility = "visible";
+  }
 
-// Hide overlay
-gsap.to(".overlay", { x: "100%", duration: .5, delay: 2.6 });
-gsap.to(".overlay", { display: "none", delay: 3.1 });
+  // Hide follower div
+  function unfollow(e) {
+    follower.style.visibility = "hidden";
+  }
 
-// Text pop up
-gsap.to(".welcome h1", { y: 70 });
-gsap.to(".welcome h1", { y: 0, opacity: 1, ease: "back.out(1.7)", duration: .7, delay: 3.1 });
+  function startVideo(e) {
+    e.target.children[2].play();
+  }
 
-// Text pop up
-gsap.to(".short-presentation p", { y: 170 });
-gsap.to(".short-presentation p", { y: 0, opacity: 1, ease: "back.out(1)", duration: .7, delay: 3.1 });
+  function pauseVideo(e) {
+    e.target.nextElementSibling.nextElementSibling.pause();
+  }
 
-// Project pages, grab first link button.
-const showPage = document.querySelector(".action-buttons a");
+  // Event listeners
+  projectLinks.forEach(item => item.addEventListener("mousemove", follow));
+  projectLinks.forEach(item => item.addEventListener("mouseout", unfollow));
+  computers.forEach(item => item.addEventListener("mouseenter", startVideo));
+  computers.forEach(item => item.addEventListener("mouseout", pauseVideo));
 
-// Add hover effect to the link button
-showPage.addEventListener("mouseover", function (e) {
-  gsap.to(showPage, { scale: 0.5, duration: 0.1 });
-  gsap.to(showPage, { scale: 1, duration: 0.1, delay: 0.1 });
-});
+  // Scroll reveal defaults
+  window.sr = ScrollReveal({
+    duration: 600,
+    easing: "ease-in",
+    viewFactor: 0.3
+  });
 
-// Project page, the downarrow under the description text
-// add infinite animation to that arrow.
-var arrowTl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
-arrowTl.to(".downarr", { x: -10, delay: 0.5, duration: 0.5 });
-arrowTl.to(".downarr", { opacity: 0, duration: 0.3 }, "-=0.2");
-arrowTl.to(".downarr", { x: 0 });
-arrowTl.to(".downarr", { opacity: 1, duration: 0.3 });
+  // Reveal on scroll
+  sr.reveal('.background');
+  sr.reveal(".skills");
+  sr.reveal(".show", { duration: 900, easing: "ease", viewFactor: 0.1 });
 
-// Project page, when you start scrolling make description text
-// disappear
-var controller = new ScrollMagic.Controller();
+  // Show loader
+  gsap.to(".loader-container", { opacity: 1, delay: 0.5, duration: 1 });
 
-var tween = gsap.to(".scrollmore", { opacity: 0, duration: .7 });
+  // Progress bar running from 0 - 100%
+  gsap.to(".loader-show", { x: "105%", delay: 1.6, duration: 1, ease: "power2.out" });
 
-var scene = new ScrollMagic.Scene({
-  triggerElement: ".image",
-  triggerHook: 0.35
-})
-  .setTween(tween)
-  .addTo(controller);
+  // Hide overlay
+  gsap.to(".overlay", { x: "100%", duration: .5, delay: 2.6 });
+  gsap.to(".overlay", { display: "none", delay: 3.1 });
+
+  // Text pop up
+  gsap.to(".welcome h1", { y: 70 });
+  gsap.to(".welcome h1", { y: 0, opacity: 1, ease: "back.out(1.7)", duration: .7, delay: 3.1 });
+
+  // Text pop up
+  gsap.to(".short-presentation p", { y: 170 });
+  gsap.to(".short-presentation p", { y: 0, opacity: 1, ease: "back.out(1)", duration: .7, delay: 3.1 });
+}
+
+
+
+if (window.location.href.indexOf("pages") > -1) {
+  // Project pages, grab first link button.
+  const showPage = document.querySelector(".action-buttons a");
+
+  // Add hover effect to the link button
+  showPage.addEventListener("mouseover", function (e) {
+    gsap.to(showPage, { scale: 0.5, duration: 0.1 });
+    gsap.to(showPage, { scale: 1, duration: 0.1, delay: 0.1 });
+  });
+
+  // Project page, the downarrow under the description text
+  // add infinite animation to that arrow.
+  var arrowTl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+  arrowTl.to(".downarr", { x: -10, delay: 0.5, duration: 0.5 });
+  arrowTl.to(".downarr", { opacity: 0, duration: 0.3 }, "-=0.2");
+  arrowTl.to(".downarr", { x: 0 });
+  arrowTl.to(".downarr", { opacity: 1, duration: 0.3 });
+
+  // Project page, when you start scrolling make description text
+  // disappear
+  var controller = new ScrollMagic.Controller();
+
+  var tween = gsap.to(".scrollmore", { opacity: 0, duration: .7 });
+
+  var scene = new ScrollMagic.Scene({
+    triggerElement: ".image",
+    triggerHook: 0.35
+  })
+    .setTween(tween)
+    .addTo(controller);
+}
 
 
 
